1b87303b83372ebc47f3289866429c47d9022d84
Mohammad Sumon <sumon3011@gmail.com>
2022-12-10 13:03:59 +0600

    my education and job experience are done

--- src/component/Resume/Resume.js
+++ src/component/Resume/Resume.js
@@ -9,14 +9,14 @@ const Resume = () => {
       <section className='Resume' id='resume'>
         <div className='container top'>
           <div className='heading text-center'>
-            <h4>7+ YEARS OF EXPERIENCE</h4>
+            {/* <h4>7+ YEARS OF EXPERIENCE</h4> */}
             <h1>My Resume</h1>
           </div>
 
           <div className='content-section mtop d_flex'>
             <div className='left'>
               <div className='heading'>
-                <h4>2007-2010</h4>
+                {/* <h4>2007-2010</h4> */}
                 <h1>Education Quality</h1>
               </div>
 
@@ -44,7 +44,7 @@ const Resume = () => {
             </div>
             <div className='left'>
               <div className='heading'>
-                <h4>2007-2010</h4>
+                {/* <h4>2007-2010</h4> */}
                 <h1>Job Experience</h1>
               </div>
 
