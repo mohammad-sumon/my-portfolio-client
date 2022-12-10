20faf5a546608ec94aed509c2d47372c3b14f66c
Mohammad Sumon <sumon3011@gmail.com>
2022-12-10 12:50:05 +0600

    main page structure done

---
+++ src/component/Features/Features-Api.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    // image: "https://img.icons8.com/ios/64/000000/menu.png",
+    title: "Used Resale Product",
+    desc: "Here you can find used resale products collection. You can buy your desired product at the best price.",
+  },
+  {
+    id: 2,
+    // image: "https://img.icons8.com/ios/40/000000/open-book.png",
+    title: "SAM Kitchen",
+    desc: "This is a website of food lovers. All items are homemade and maintain hyzienic.",
+  },
+  {
+    id: 3,
+    // image: "https://img.icons8.com/fluency-systems-regular/64/000000/retro-tv.png ",
+    title: "Choose Five Legend",
+    desc: "From this webiste you can choose your best five football legend. You also can calculate your budget.",
+  },
+  // {
+  //   id: 4,
+  //   // image: "https://img.icons8.com/material-outlined/64/000000/topic--v1.png ",
+  //   title: "Mobile App",
+  //   desc: "There are many variations of passages of Lorem Ipsum	available, but the majority.",
+  // },
+  // {
+  //   id: 5,
+  //   // image: "https://img.icons8.com/ios-filled/64/000000/wifi.png ",
+  //   title: "CEO Marketing",
+  //   desc: "always free from repetition, injected humour, or non-characteristic words etc.",
+  // },
+  // {
+  //   id: 6,
+  //   // image: "https://img.icons8.com/glyph-neue/50/000000/polyline.png ",
+  //   title: "Personal Portfolio April",
+  //   desc: " It uses a dictionary of over 200 Latin words, combined with a handful of model sentence.",
+  // },
+]
+export default data
---
+++ src/component/Hero/Home.js
@@ -0,0 +1,69 @@
+import React from "react"
+import "./Home.css"
+import hero from "../pic/sumon.jpg"
+// import skill1 from "../pic/skill1.png"
+// import skill2 from "../pic/skill2.png"
+// import skill3 from "../pic/skill3.png"
+import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa"
+// import { Typewriter } from "react-simple-typewriter"
+
+const Home = () => {
+  return (
+    <>
+      <section className='hero' id='home'>
+        <div className='container f_flex top'>
+          <div className='left top'>
+            <h3>WELCOME TO MY PORTFOLIO</h3>
+            <h1>
+              Hi, I'm <span>Md. Sumon</span>
+            </h1>
+            <h2>
+              {/* a */}
+              <span>
+                {/* <Typewriter words={[" Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} /> */}
+              </span>
+            </h2>
+
+            <p>I am a Front-End Web Developer. You can find lot of responsive projects with the latest technologies. Coding is my passion. You also can connect with me in different ways.</p>
+
+            <div className='hero_btn d_flex'>
+              <div className='col_1'>
+                <h4>FIND WITH ME</h4>
+                <div className='button'>
+                  <button className='btn_shadow'>
+                    <FaFacebook></FaFacebook>
+                  </button>
+                  <button className='btn_shadow'>
+                    <FaInstagram></FaInstagram>
+                  </button>
+                  <button className='btn_shadow'>
+                    <FaLinkedin></FaLinkedin>
+                  </button>
+                </div>
+              </div>
+              {/* <div className='col_1'>
+                <h4>BEST SKILL ON</h4>
+                <button className='btn_shadow'>
+                  <img src={skill1} alt='' />
+                </button>
+                <button className='btn_shadow'>
+                  <img src={skill2} alt='' />
+                </button>
+                <button className='btn_shadow'>
+                  <img src={skill3} alt='' />
+                </button>
+              </div> */}
+            </div>
+          </div>
+          <div className='right'>
+            <div className='right_img'>
+              <img src={hero} alt='' />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
