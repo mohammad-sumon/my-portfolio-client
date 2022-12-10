f21bd36356241e1b834d91a0fc720cd9cff23c87
Mohammad Sumon <sumon3011@gmail.com>
2022-12-10 16:42:54 +0600

    primary works done

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
-import contact1 from "./contact2.jpg"
-import "./Contact.css"
-import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa"
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState } from "react";
+import contact1 from "./contact2.jpg";
+import "./Contact.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 
 const Contact = () => {
   const [data, setData] = useState({
@@ -11,70 +12,80 @@ const Contact = () => {
     phone: "",
     email: "",
     subject: "",
-    message: "",})
+    message: "",
+  });
 
   const InputEvent = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setData((preVal) => {
       return {
         ...preVal,
         [name]: value,
-      }
-    })
-  }
+      };
+    });
+  };
   const form = useRef();
 
   const formSubmit = (event) => {
-    
     event.preventDefault();
-    emailjs.sendForm('service_kfp8fqd', 'template_q9ukzza', form.current, 'kcg6GYtARzLDBv0hF')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_kfp8fqd",
+        "template_q9ukzza",
+        form.current,
+        "kcg6GYtARzLDBv0hF"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           // console.log('test message ok');
           // setData();
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-    alert(
-      `My name is ${data.fullname}. 
-	My phone number is ${data.phone}. 
-	My email address is ${data.email}. 
-	My Subject on  ${data.subject}. 
-	Here is my message I want to say : ${data.message}. 
-	`
-    )
-  }
+        }
+      );
+      
+      toast.success('Successfully Submit');
+      event.target.reset();
+  
+  };
+
   return (
     <>
-      <section className='Contact' id='contact'>
-        <div className='container top'>
-          <div className='heading text-center'>
+      <section className="Contact" id="contact">
+        <div className="container top">
+          <div className="heading text-center">
             <h4>CONTACT</h4>
             <h1>Contact With Me</h1>
           </div>
 
-          <div className='content d_flex'>
-            <div className='left'>
-              <div className='box box_shodow'>
-                <div className='img'>
-                  <img src={contact1} alt='' style={{width: "400px"}} />
+          <div className="content d_flex">
+            <div className="left">
+              <div className="box box_shodow">
+                <div className="img">
+                  <img src={contact1} alt="" style={{ width: "400px" }} />
                 </div>
-                <div className='details'>
+                <div className="details">
                   <h1>Md. Sumon</h1>
                   <p>Front-End Web Developer</p>
-                  <p>Please feel free to contact with me. Connect with me via and call in to my account.</p> <br />
+                  <p>
+                    Please feel free to contact with me. Connect with me via and
+                    call in to my account.
+                  </p>{" "}
+                  <br />
                   <p>Phone: +880 16111 56780</p>
                   <p>Email: sumon3011@gmail.com</p> <br />
                   <span>FIND WITH ME</span>
-                  <div className='button f_flex'>
-                    <button className='btn_shadow'>
+                  <div className="button f_flex">
+                    <button className="btn_shadow">
                       <FaFacebook></FaFacebook>
                     </button>
-                    <button className='btn_shadow'>
+                    <button className="btn_shadow">
                       <FaInstagram></FaInstagram>
                     </button>
-                    <button className='btn_shadow'>
+                    <button className="btn_shadow">
                       <FaLinkedin></FaLinkedin>
                     </button>
                   </div>
@@ -82,32 +93,63 @@ const Contact = () => {
               </div>
             </div>
 
-            <div className='right box_shodow'>
+            <div className="right box_shodow">
               <form ref={form} onSubmit={formSubmit}>
-                <div className='f_flex'>
-                  <div className='input row'>
+                <div className="f_flex">
+                  <div className="input row">
                     <span>YOUR NAME</span>
-                    <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
+                    <input
+                      required
+                      type="text"
+                      name="fullname"
+                      value={data.fullname}
+                      onChange={InputEvent}
+                    />
                   </div>
-                  <div className='input row'>
+                  <div className="input row">
                     <span>PHONE NUMBER </span>
-                    <input type='number' name='phone' value={data.phone} onChange={InputEvent} />
+                    <input
+                      required
+                      type="number"
+                      name="phone"
+                      value={data.phone}
+                      onChange={InputEvent}
+                    />
                   </div>
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>EMAIL </span>
-                  <input type='email' name='email' value={data.email} onChange={InputEvent} />
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    onChange={InputEvent}
+                  />
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>SUBJECT </span>
-                  <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
+                  <input
+                    required
+                    type="text"
+                    name="subject"
+                    value={data.subject}
+                    onChange={InputEvent}
+                  />
                 </div>
-                <div className='input'>
+                <div className="input">
                   <span>YOUR MESSAGE </span>
-                  <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
+                  <textarea
+                    required
+                    cols="30"
+                    rows="10"
+                    name="message"
+                    value={data.message}
+                    onChange={InputEvent}
+                  ></textarea>
                 </div>
-                <button type="submit" className='btn_shadow'>
-                  SEND MESSAGE <i className='fa fa-long-arrow-right'></i>
+                <button type="submit" className="btn_shadow">
+                  SEND MESSAGE <i className="fa fa-long-arrow-right"></i>
                 </button>
               </form>
             </div>
@@ -115,7 +157,7 @@ const Contact = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
