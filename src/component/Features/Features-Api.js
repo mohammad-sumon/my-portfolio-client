4b73a19981ce4136579ac86c5610a4cb2adb7e06
Mohammad Sumon <sumon3011@gmail.com>
2022-12-11 17:35:40 +0600

    projects data added

--- src/component/Features/Features-Api.js
+++ src/component/Features/Features-Api.js
@@ -3,6 +3,9 @@ const data = [
   {
     id: 1,
     image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    image1: "https://i.ibb.co/1vK67jP/ac1.png",
+    image2: "https://i.ibb.co/qyy2sGV/Screenshot-1.png",
+    image3: "https://i.ibb.co/nzLjx7g/Screenshot-2.png",
     title: "Used Resale Product",
     desc: "Here you can find used resale products collection. You can buy your desired product at the best price.",
   },
--- src/component/Features/Project.js
+++ src/component/Features/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import data from "./Features-Api";
+import './Project.css';
 // import data from "./Features-Api"
 
 const Project = (props) => {
@@ -11,12 +12,31 @@ const Project = (props) => {
   // console.log(props[0]);
 
   return (
-    <div>
+    <div className="projectDiv">
       <h2>This is single project</h2>
       <div>
         <h3>Project Name: {data[0]?.title}</h3>
-        <img src={data[0]?.image} alt="" />
+        <div className="projectImg">
+        <img src={data[0]?.image1} alt="" />
+        <img src={data[0]?.image2} alt="" />
+        <img src={data[0]?.image3} alt="" />
+        </div> 
         <p>Desctiption: {data[0]?.desc}</p>
+        <h3>Features & Functionality:</h3>
+        <ol>
+          <li>1. React Router Dom</li>
+          <li>2. Firebase Authentication</li>
+          <li>3. React Query</li>
+          <li>4. CRUD Operation</li>
+          <li>5. Used backend server in Vercel</li>
+          <li>6. Generic Modal</li>
+        </ol>
+        <div className="myCode">
+        <a href="https://github.com/mohammad-sumon/my-twelfth-assignment-client">Github Client Code</a>
+        <a href="https://github.com/mohammad-sumon/my-twelfth-assignment-server">Github Server Code</a>
+        <a href="https://used-products-resale-cli-8eb57.web.app/">Live Website</a>
+        </div>
+        
       </div>
       
     </div>
